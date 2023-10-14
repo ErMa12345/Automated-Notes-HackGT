b72022ea7bf182bbea74fcd30daca1fc9289267f
emilyliublair <emilyliu.us@gmail.com>
2023-10-14 02:31:53 -0400

    audio recorder

---
+++ notes/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Recorder from './components/Recorder.js'
+
+function App() {
+  return (
+    <div className="App">
+      <Recorder /> 
+    </div>
+  );
+}
+
+export default App;
---
+++ notes/src/components/Recorder.js
@@ -0,0 +1,33 @@
+import * as React from 'react';
+import { AudioRecorder } from 'react-audio-voice-recorder';
+
+export default function Recorder() {
+  const addAudioElement = (blob) => {
+    const url = URL.createObjectURL(blob);
+    const audio = document.createElement('audio');
+    audio.src = url;
+    audio.controls = true;
+    document.body.appendChild(audio);
+  };
+
+  return (
+    <div>
+      <AudioRecorder
+        onRecordingComplete={addAudioElement}
+        audioTrackConstraints={{
+          noiseSuppression: true,
+          echoCancellation: true,
+        }}
+        onNotAllowedOrFound={(err) => console.table(err)}
+        downloadOnSavePress={true}
+        downloadFileExtension="mp3"
+        mediaRecorderOptions={{
+          audioBitsPerSecond: 128000,
+        }}
+        showVisualizer={true}
+      />
+      <br />
+    </div>
+  );
+}
+
