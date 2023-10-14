67b9107b428d56f6c3acd256000bb6c22683589f
Eric Ma <er11cma2004@gmail.com>
2023-10-13 23:03:28 -0400

    Uncommented stuff

--- transcribe.py
+++ transcribe.py
@@ -2,16 +2,11 @@ import whisper
 
 model = whisper.load_model("base.en")
 
-mp3 = "testFiles\y2mate.com - Smash Ultimate Art of Incineroar.mp3"
+mp3 = "TestFiles\y2mate.com - Smash Ultimate Art of Incineroar.mp3"
 result = model.transcribe(mp3, language = "en", fp16 = False)
 print(result["text"])
-# print(len(result))
 
-# file_path = 'example.txt'
-
-# with open(file_path, 'w') as file:
-#     # Write content to the file
-#     file.write(result.text)
-   
-# print(f"Text has been written to {file_path}")
-
+file_path = "Transcriptions\TranscribedAudio.txt"
+with open(file_path, 'w') as file:
+    # Write content to the file
+    file.write(result["text"])
